cfa9f001b631cc457809cbc5009273ddab3a654c
350HP <33990105+350HP@users.noreply.github.com>
2018-01-25 15:50:53 -0600

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,3 +1,7 @@
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Desktop;
+import java.awt.Dimension;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
@@ -15,6 +19,13 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Objects;
 
+import javax.swing.JFrame;
+import javax.swing.JTextPane;
+import javax.swing.text.BadLocationException;
+import javax.swing.text.Style;
+import javax.swing.text.StyleConstants;
+import javax.swing.text.StyledDocument;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -26,7 +37,7 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 public class Main {
 
 	// Class to hold individual offer
-	static class Offer implements Comparable<Offer>{
+	static class Offer implements Comparable<Offer> {
 		public String MainOffer;
 		public String Merchant;
 		public Date ExpiryDate;
@@ -83,151 +94,214 @@ public class Main {
 		}
 	}
 
-	public static void main(String[] args) throws IOException {
+	public static void addColoredText(JTextPane pane, String text, Color color) {
+		StyledDocument doc = pane.getStyledDocument();
+
+		Style style = pane.addStyle("Color Style", null);
+		StyleConstants.setForeground(style, color);
+		try {
+			doc.insertString(doc.getLength(), text, style);
+		} catch (BadLocationException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void main(String[] args) throws IOException, InterruptedException {
+		// Simple GUI for status
+		JFrame frame = new JFrame();
+		JTextPane pane = new JTextPane();
+		;
+
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setTitle("Amex Offers");
+		pane.setPreferredSize(new Dimension(350, 200));
+		frame.getContentPane().add(pane, BorderLayout.CENTER);
+		frame.pack();
+		frame.setVisible(true);
+
 		// Open a CSV file to output information
+		addColoredText(pane, "Creating file to store data...\n", Color.BLACK);
 		DateFormat dateFormat = new SimpleDateFormat("MMM-dd hh-mm-a");
 		PrintWriter printWriter = null;
+		String outputFileName = "Amex Offers " + dateFormat.format(new Date()) + ".csv";
 		try {
-			printWriter = new PrintWriter(new File("Amex Offers " + dateFormat.format(new Date()) + ".csv"));
+			printWriter = new PrintWriter(new File(outputFileName));
 		} catch (FileNotFoundException e) {
-			e.printStackTrace();
+			addColoredText(pane, "There was a problem while creating the file. Exiting now.\n", Color.RED);
+			Thread.sleep(4000);
+			System.exit(0);
 		}
 
 		// Extract the required Chrome Driver and set it up
-		String OS = System.getProperty("os.name");
-		String chromeDriverFileName;
+		addColoredText(pane, "Launching Chrome..\n", Color.BLACK);
+		String chromeDriverFileName = null;
+		WebDriver driver = null;
+		WebDriverWait wait = null;
+		File filetoDelete = null;
+		try {
+			String OS = System.getProperty("os.name");
 
-		if (OS.contains("Windows"))
-			chromeDriverFileName = "chromedriver.exe";
-		else if (OS.contains("Mac OS"))
-			chromeDriverFileName = "chromedriver.mac";
-		else
-			chromeDriverFileName = "chromedriver.linux";
+			if (OS.contains("Windows"))
+				chromeDriverFileName = "chromedriver.exe";
+			else if (OS.contains("Mac OS"))
+				chromeDriverFileName = "chromedriver.mac";
+			else
+				chromeDriverFileName = "chromedriver.linux";
 
-		InputStream res = Main.class.getResourceAsStream("/" + chromeDriverFileName);
-		FileOutputStream output = new FileOutputStream(chromeDriverFileName);
-		byte[] buffer = new byte[4096];
-		int bytesRead = res.read(buffer);
-		while (bytesRead != -1) {
-			output.write(buffer, 0, bytesRead);
-			bytesRead = res.read(buffer);
+			InputStream res = Main.class.getResourceAsStream("/" + chromeDriverFileName);
+			FileOutputStream output = new FileOutputStream(chromeDriverFileName);
+			byte[] buffer = new byte[4096];
+			int bytesRead = res.read(buffer);
+			while (bytesRead != -1) {
+				output.write(buffer, 0, bytesRead);
+				bytesRead = res.read(buffer);
+			}
+			output.close();
+			res.close();
+
+			if (OS.contains("Windows"))
+				System.setProperty("webdriver.chrome.driver",
+						System.getProperty("user.dir") + "\\" + chromeDriverFileName);
+			else {
+				File f = new File(chromeDriverFileName);
+				f.setExecutable(true, true);
+				System.setProperty("webdriver.chrome.driver",
+						System.getProperty("user.dir") + "/" + chromeDriverFileName);
+			}
+
+			ChromeOptions options = new ChromeOptions();
+			options.addArguments("--start-maximized");
+
+			driver = new ChromeDriver(options);
+			wait = new WebDriverWait(driver, 20);
+		} catch (Exception e) {
+			addColoredText(pane, "There was a problem while launching Chrome. Exiting now\n", Color.RED);
+			driver.quit();
+			filetoDelete = new File(chromeDriverFileName);
+			filetoDelete.delete();
+			printWriter.close();
+			filetoDelete = new File(outputFileName);
+			filetoDelete.delete();
+			Thread.sleep(4000);
+			System.exit(0);
 		}
-		output.close();
-		res.close();
-
-		if (OS.contains("Windows"))
-			System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "\\" + chromeDriverFileName);
-		else {
-			File f = new File(chromeDriverFileName);
-			f.setExecutable(true, true);
-			System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "/" + chromeDriverFileName);
-		}
-
-		ChromeOptions options = new ChromeOptions();
-		options.addArguments("--start-maximized");
-
-		WebDriver driver = new ChromeDriver(options);
-		WebDriverWait wait = new WebDriverWait(driver, 20);
 
 		// Start Processing Offers
+		addColoredText(pane, "Processing your offers..\n", Color.BLACK);
 		StringBuilder builder = new StringBuilder();
-		driver.get("https://global.americanexpress.com/accounts");
-		wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//span[text()='Add Another Card']")));
-
-		int noOfCards = driver.findElements(By.xpath("//div[starts-with(@class,'app__card__')]")).size();
-
 		ArrayList<Offer> offersList = new ArrayList<Offer>();
 		ArrayList<String> cardsList = new ArrayList<String>();
 		HashMap<Offer, String> offersRelation = new HashMap<Offer, String>();
-
-		for (int i = 0; i < noOfCards; i++) {
-			List<WebElement> allCards = driver.findElements(By.xpath("//div[starts-with(@class,'app__card__')]"));
-			WebElement card = allCards.get(i);
-
-			String cardName = card.findElement(By.xpath(".//span[@class='heading-3']")).getText();
-			cardsList.add(cardName);
-
-			WebElement selectCard = card
-					.findElement(By.xpath(".//button[starts-with(@class,'btn-block btn-block-center')]"));
-
-			if (selectCard.getText().contains("Account Selected")) {
-				WebElement backButton = driver.findElement(
-						By.xpath("//a[starts-with(@class,'main-menu dls-accent-white-01 position-absolute')]"));
-				backButton.click();
-			} else {
-				selectCard.click();
-			}
-
-			wait.until(ExpectedConditions
-					.elementToBeClickable(By.xpath("//span[starts-with(@class,'GlobalHeader__closed___')]")));
-			wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//a[text()='Frequently Asked Questions']")));
-
-			// after page loaded
-			if (!driver.getCurrentUrl().contains("offers")) {
-				WebElement viewAll = driver.findElement(By.xpath("//a[text()='View All']"));
-				wait.until(ExpectedConditions.elementToBeClickable(viewAll));
-				try {
-					Thread.sleep(1000);
-				} catch (InterruptedException ex) {
-					ex.printStackTrace();
-				}
-				viewAll.click();
-			}
-
-			wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//a[text()='Frequently Asked Questions']")));
-
-			// process eligible offers
-			driver.findElement(By.xpath("//a[@data-view-name='ELIGIBLE']")).click();
-			wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//a[text()='Frequently Asked Questions']")));
-			List<WebElement> offers = driver
-					.findElements(By.xpath("//section[@class='offers-list']/section/div[starts-with(@id,'offer-')]"));
-			for (WebElement currentOfferElement : offers) {
-				Offer TempOffer = new Offer(
-						currentOfferElement.findElement(By.xpath(".//p[starts-with(@class,'heading-3')]")).getText(),
-						currentOfferElement.findElement(By.xpath(".//p[starts-with(@class,'body-1')]")).getText(),
-						currentOfferElement.findElement(By.xpath(".//div[starts-with(@class,'offer-expires')]"))
-								.getText());
-				if (!offersList.contains(TempOffer))
-					offersList.add(TempOffer);
-
-				offersRelation.put(TempOffer,
-						(offersRelation.get(TempOffer) == null ? "" : offersRelation.get(TempOffer))
-								+ cardsList.indexOf(cardName) + ",ELIGIBLE;");
-			}
-
-			// process enrolled offers
-			driver.findElement(By.xpath("//a[@data-view-name='ENROLLED']")).click();
-			wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//a[text()='Frequently Asked Questions']")));
-			offers = driver
-					.findElements(By.xpath("//section[@class='offers-list']/section/div[starts-with(@id,'offer-')]"));
-			for (WebElement currentOfferElement : offers) {
-				Offer TempOffer = new Offer(
-						currentOfferElement.findElement(By.xpath(".//p[starts-with(@class,'heading-3')]")).getText(),
-						currentOfferElement.findElement(By.xpath(".//p[starts-with(@class,'body-1')]")).getText(),
-						currentOfferElement.findElement(By.xpath(".//div[starts-with(@class,'offer-expires')]"))
-								.getText());
-				if (!offersList.contains(TempOffer))
-					offersList.add(TempOffer);
-
-				offersRelation.put(TempOffer,
-						(offersRelation.get(TempOffer) == null ? "" : offersRelation.get(TempOffer))
-								+ cardsList.indexOf(cardName) + ",ENROLLED;");
-			}
-
-			if (i == noOfCards - 1)
-				break;
-
+		try {
 			driver.get("https://global.americanexpress.com/accounts");
 			wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//span[text()='Add Another Card']")));
 
-		}
+			int noOfCards = driver.findElements(By.xpath("//div[starts-with(@class,'app__card__')]")).size();
 
-		driver.quit();
-		File file = new File(chromeDriverFileName);
-		file.delete();
+			
+			for (int i = 0; i < noOfCards; i++) {
+				List<WebElement> allCards = driver.findElements(By.xpath("//div[starts-with(@class,'app__card__')]"));
+				WebElement card = allCards.get(i);
+
+				String cardName = card.findElement(By.xpath(".//span[@class='heading-3']")).getText();
+				if (cardName.contains("Savings")) continue; //skip savings
+				cardsList.add(cardName);
+
+				WebElement selectCard = card
+						.findElement(By.xpath(".//button[starts-with(@class,'btn-block btn-block-center')]"));
+
+				if (selectCard.getText().contains("Account Selected")) {
+					WebElement backButton = driver.findElement(
+							By.xpath("//a[starts-with(@class,'main-menu dls-accent-white-01 position-absolute')]"));
+					backButton.click();
+				} else {
+					selectCard.click();
+				}
+
+				wait.until(ExpectedConditions
+						.elementToBeClickable(By.xpath("//span[starts-with(@class,'GlobalHeader__closed___')]")));
+				wait.until(
+						ExpectedConditions.elementToBeClickable(By.xpath("//a[text()='Frequently Asked Questions']")));
+
+				// after page loaded
+				if (!driver.getCurrentUrl().contains("offers")) {
+					WebElement viewAll = driver.findElement(By.xpath("//a[text()='View All']"));
+					wait.until(ExpectedConditions.elementToBeClickable(viewAll));
+					Thread.sleep(1000);
+					viewAll.click();
+				}
+
+				wait.until(
+						ExpectedConditions.elementToBeClickable(By.xpath("//a[text()='Frequently Asked Questions']")));
+
+				// process eligible offers
+				driver.findElement(By.xpath("//a[@data-view-name='ELIGIBLE']")).click();
+				wait.until(
+						ExpectedConditions.elementToBeClickable(By.xpath("//a[text()='Frequently Asked Questions']")));
+				List<WebElement> offers = driver.findElements(
+						By.xpath("//section[@class='offers-list']/section/div[starts-with(@id,'offer-')]"));
+				for (WebElement currentOfferElement : offers) {
+					Offer TempOffer = new Offer(
+							currentOfferElement.findElement(By.xpath(".//p[starts-with(@class,'heading-3')]"))
+									.getText(),
+							currentOfferElement.findElement(By.xpath(".//p[starts-with(@class,'body-1')]")).getText(),
+							currentOfferElement.findElement(By.xpath(".//div[starts-with(@class,'offer-expires')]"))
+									.getText());
+					if (!offersList.contains(TempOffer))
+						offersList.add(TempOffer);
+
+					offersRelation.put(TempOffer,
+							(offersRelation.get(TempOffer) == null ? "" : offersRelation.get(TempOffer))
+									+ cardsList.indexOf(cardName) + ",ELIGIBLE;");
+				}
+
+				// process enrolled offers
+				driver.findElement(By.xpath("//a[@data-view-name='ENROLLED']")).click();
+				wait.until(
+						ExpectedConditions.elementToBeClickable(By.xpath("//a[text()='Frequently Asked Questions']")));
+				offers = driver.findElements(
+						By.xpath("//section[@class='offers-list']/section/div[starts-with(@id,'offer-')]"));
+				for (WebElement currentOfferElement : offers) {
+					Offer TempOffer = new Offer(
+							currentOfferElement.findElement(By.xpath(".//p[starts-with(@class,'heading-3')]"))
+									.getText(),
+							currentOfferElement.findElement(By.xpath(".//p[starts-with(@class,'body-1')]")).getText(),
+							currentOfferElement.findElement(By.xpath(".//div[starts-with(@class,'offer-expires')]"))
+									.getText());
+					if (!offersList.contains(TempOffer))
+						offersList.add(TempOffer);
+
+					offersRelation.put(TempOffer,
+							(offersRelation.get(TempOffer) == null ? "" : offersRelation.get(TempOffer))
+									+ cardsList.indexOf(cardName) + ",ENROLLED;");
+				}
+
+				if (i == noOfCards - 1)
+					break;
+
+				driver.get("https://global.americanexpress.com/accounts");
+				wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//span[text()='Add Another Card']")));
+
+			}
+		} catch (Exception e) {
+			addColoredText(pane, "There was a problem while processing your offers. Exiting now\n", Color.RED);
+			printWriter.close();
+			filetoDelete = new File(outputFileName);
+			filetoDelete.delete();
+			Thread.sleep(4000);
+			driver.quit();
+			filetoDelete = new File(chromeDriverFileName);
+			filetoDelete.delete();
+			System.exit(0);
+		} finally {
+			driver.quit();
+			filetoDelete = new File(chromeDriverFileName);
+			filetoDelete.delete();
+		}
 
 		// start building the output file
 		// header
+		addColoredText(pane, "Saving your offers to file..\n", Color.BLACK);
 		builder.append("Offer,Merchant,Date");
 		for (String s : cardsList) {
 			builder.append("," + s);
@@ -247,7 +321,7 @@ public class Main {
 				TempMap.put(Integer.parseInt(tempsplit[0]), tempsplit[1]);
 			}
 
-			for (int i = 0; i < noOfCards; i++) {
+			for (int i = 0; i < cardsList.size(); i++) {
 				if (TempMap.containsKey(i))
 					builder.append("," + TempMap.get(i));
 				else
@@ -257,7 +331,20 @@ public class Main {
 		}
 		printWriter.write(builder.toString());
 		printWriter.close();
-
+		addColoredText(pane, "Successfully completed. Exiting now.", Color.GREEN);
+		
+		//launch file
+		if (Desktop.isDesktopSupported()) {
+		    try {
+		        File outputFile = new File(outputFileName);
+		        Desktop.getDesktop().open(outputFile);
+		    } catch (IOException ex) {
+		        // no application registered for CSV
+		    }
+		}
+		
+		Thread.sleep(2000);
+		return;
 	}
 
 }
